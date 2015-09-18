5f6681e3562ad054d8d9b615ddb00b6f285f320e
Mohd Sanusi <sanusi@jenjobs.com>
2015-09-18 14:58:18 +0800

    rename post parameter

--- upload.php
+++ upload.php
@@ -1,8 +1,9 @@
 <?php
-	$fileName = explode( '.', $_POST['filename'] );
+	$fileName = explode( '.', $_POST['file_name'] );
 	$fileNameExtension = end( $fileName );
-	
-	$handle2 = fopen( time().".$fileNameExtension", 'w' );
+	$newFileName = time().".".$fileNameExtension;
+	echo $newFileName;
+	$handle2 = fopen( $newFileName, 'w' );
 	$b = fwrite( $handle2, base64_decode( $_POST['base64Str'] ) );
 	fclose( $handle2 );
 ?>
